f608843b0278e42028f17a4447b5582e6ee69f33
DevNebulae <gamerkeeperson+github@gmail.com>
2018-05-21 11:13:09 +0200

    Add documentation to SubRoute interface

--- src/main/kotlin/com/rekeningrijden/simulation/route/SubRoute.kt
+++ src/main/kotlin/com/rekeningrijden/simulation/route/SubRoute.kt
@@ -4,10 +4,29 @@ import com.rekeningrijden.simulation.coordinate.Coordinate
 import java.util.LinkedList
 
 interface SubRoute {
+    /**
+     * A set amount of coordinates which are bound by a country's borders. A
+     * [Route][com.rekeningrijden.simulation.route.Route] is composed of
+     * multiple sub-routes. Only countries which have registered with the
+     * rekeningrijden program will have routes registered.
+     */
     val coordinates: LinkedList<Coordinate>
+
+    /**
+     * The
+     * [ISO 3166-1 alpha-2](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2)
+     * country code which indicates which country the car originates from.
+     */
     val countryCode: String
+
+    /**
+     * TODO("Refactor away in favor of Iteratrors")
+     */
     var isSubRouteDriven: Boolean
 
+    /**
+     * TODO("Refactor away in favor of Iteratrors")
+     */
     fun getNextCoordinateAtIndex(index: Int): Coordinate? {
         if (index >= coordinates.size) {
             this.isSubRouteDriven = true
