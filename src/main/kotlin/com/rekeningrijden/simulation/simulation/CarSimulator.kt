4db31b1e508acfb081ed1054b24f047d50a18a06
DevNebulae <gamerkeeperson+github@gmail.com>
2018-05-21 12:38:10 +0200

    Add DI to CarSimulator service

--- src/main/kotlin/com/rekeningrijden/simulation/simulation/CarSimulator.kt
+++ src/main/kotlin/com/rekeningrijden/simulation/simulation/CarSimulator.kt
@@ -3,20 +3,38 @@ package com.rekeningrijden.simulation.simulation
 import com.rekeningrijden.simulation.car.Car
 import com.rekeningrijden.simulation.journey.Journey
 import com.rekeningrijden.simulation.route.Route
+import com.rekeningrijden.simulation.services.CarService
+import com.rekeningrijden.simulation.services.RouteService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
+import org.springframework.core.task.TaskExecutor
+import org.springframework.stereotype.Service
+import sun.plugin2.message.Message
 import java.util.Random
 import kotlin.properties.Delegates
 
+@Service
 class CarSimulator {
+    @Autowired
+    private lateinit var applicationContext: ApplicationContext
+
+    @Autowired
+    private lateinit var carService: CarService
+
+    @Autowired
+    private lateinit var routeService: RouteService
+
+    @Autowired
+    private lateinit var taskExecutor: TaskExecutor
+
     private var cars by Delegates.notNull<Set<Car>>()
-    private var journeys by Delegates.notNull<List<Journey>>()
     private var routes by Delegates.notNull<List<Route>>()
     private val rndm = Random()
-    private val messageProducer = MessageProducer()
 
-    fun initialize(cars: Set<Car>, routes: List<Route>) {
-        this.cars = cars
-        this.routes = routes
-        this.journeys = cars.map { Journey(this, messageProducer, it, routes[rndm.nextInt(routes.size)]) }
+
+    fun initialize() {
+        this.cars = carService.cars
+        this.routes = routeService.routes
     }
 
     val newRoute: Route
@@ -29,8 +47,12 @@ class CarSimulator {
         }
 
     fun startSimulation() {
-        for (t in journeys) {
-            t.start()
+        cars.forEach {
+            val journey = applicationContext.getBean(Journey::class.java)
+
+            journey.initialize(it, routes[rndm.nextInt(routes.size)])
+            taskExecutor.execute(journey)
+
             Thread.sleep(((rndm.nextInt(10) + 1) * 1000).toLong())
         }
     }
