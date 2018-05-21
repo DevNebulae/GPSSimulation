2dde141cae349e8a7bcd5d129af6d91253c70b05
DevNebulae <gamerkeeperson+github@gmail.com>
2018-05-21 21:22:31 +0200

    Format CarCommandLineRunner according to ktlint

--- src/main/kotlin/com/rekeningrijden/simulation/runners/CarCommandLineRunner.kt
+++ src/main/kotlin/com/rekeningrijden/simulation/runners/CarCommandLineRunner.kt
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.core.annotation.Order
 import org.springframework.core.io.ClassPathResource
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.stereotype.Component
 
 @Component
