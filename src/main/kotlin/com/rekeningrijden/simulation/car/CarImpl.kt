e9a139afa85a9ccc08b6ed091fee00efc9654bb4
DevNebulae <gamerkeeperson+github@gmail.com>
2018-06-13 20:28:55 +0200

    Capitalize country codes

--- src/main/kotlin/com/rekeningrijden/simulation/car/CarImpl.kt
+++ src/main/kotlin/com/rekeningrijden/simulation/car/CarImpl.kt
@@ -5,7 +5,7 @@ import java.util.UUID
 import kotlin.properties.Delegates
 
 class CarImpl : Car, Serializable {
-    override var id by Delegates.notNull<UUID>()
-    override var country by Delegates.notNull<String>()
-    override val speed = 80.0
+    override val id: UUID by Delegates.notNull<UUID>()
+    override val country by Delegates.notNull<String>()
+    override var speed: Double = 80.0
 }
