902a0b85022127039039e6a4876cd0a22ea4f7aa
DevNebulae <gamerkeeperson+github@gmail.com>
2018-06-14 14:59:18 +0200

    Fix issue in generation script to capitalize country codes

--- scripts/generate-cars.py
+++ scripts/generate-cars.py
@@ -3,8 +3,10 @@ import json
 import uuid
 
 parser = argparse.ArgumentParser('car-generator')
-parser.add_argument('--amount', help='The amount of cars which will be generated for each country.', required=True, type=int)
-parser.add_argument('--countries', help='The countries the cars will be generated for.', nargs='+', required=True)
+parser.add_argument(
+    '--amount', help='The amount of cars which will be generated for each country.', required=True, type=int)
+parser.add_argument(
+    '--countries', help='The countries the cars will be generated for.', nargs='+', required=True)
 
 args = parser.parse_args()
 amount = args.amount
@@ -14,6 +16,6 @@ cars = list()
 
 for country in countries:
     for i in range(0, amount):
-        cars.append({ 'id': str(uuid.uuid4()), 'country': country })
+        cars.append({'id': str(uuid.uuid4()), 'country': country.upper()})
 
 print(json.dumps(cars))
